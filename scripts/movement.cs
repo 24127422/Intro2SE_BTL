7c06dce14c5043a3bd3a53c9958cf3a08910ede0
Đào Minh Khoa <dmkhoa2411@clc.fitus.edu.vn>
2026-07-14 21:06:07 +0700

    Update movement.cs

--- scripts/movement.cs
+++ scripts/movement.cs
@@ -32,10 +32,11 @@ public partial class movement : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 velocity = Velocity;
-		Vector2 direction = Input.GetVector("left", "right", "up", "down");
-		
-		if (DialogueUI.Instance != null && DialogueUI.Instance.IsTalking)
+		Vector2 direction = GetDirection();
+
+		bool isBlocked = (DialogueUI.Instance.IsTalking) || (JournalUI.Instance.Visible);
+
+		if (isBlocked)
 		{
 			Velocity = Vector2.Zero;
 			MoveAndSlide();
